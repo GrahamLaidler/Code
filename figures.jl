d8bb70a27d0834ab4aa82674ea3ca44e8fb54001
GrahamLaidler <g.laidler1@lancaster.ac.uk>
2022-07-29 22:47:55 +0100

    Initial Commit

--- figures.jl
+++ figures.jl
@@ -3,26 +3,26 @@ include("src/figurefunctions.jl")
 using Plots, Plots.PlotMeasures, LaTeXStrings
 
 ## Figure 2
-Figure2()  #plot(Figure2())
+Figure2()  
 savefig("fig/Fig2.png")
 println("Finished figure 2")
 
 ## Figure 3
-Figure3()  #plot(Figure2())
+Figure3()  
 savefig("fig/Fig3.png")
 println("Finished figure 3")
 
 ## Figure 4
-Figure4()  #plot(Figure2())
+Figure4()  
 savefig("fig/Fig4.png")
 println("Finished figure 4")
 
 ## Figure 5
-Figure5()  #plot(Figure2())
+Figure5()  
 savefig("fig/Fig5.png")
 println("Finished figure 5")
 
 ## Figure 6
-Figure6()  #plot(Figure2())
+Figure6()  
 savefig("fig/Fig6.png")
 println("Finished figure 6")
